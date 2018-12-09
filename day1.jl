90a9af0e0d02186e05ca0687a3086238c3efe8f4
Mike Dalessio <mike.dalessio@gmail.com>
2018-12-09 15:49:21 -0500

    day 1: prefactor for star 2

--- day1.jl
+++ day1.jl
@@ -1,17 +1,24 @@
 using Test
 
-function new_frequency(change_string)
-    accumulate(+, map(change -> parse(Int32, change),
-                      split(change_string, r"[,\s]+", keepempty=false))
-               )[end]
+function freq_changes_str_to_arr(change_string)
+    map(change -> parse(Int32, change),
+        split(change_string, r"[,\s]+", keepempty=false))
+end
+
+function next_frequency_impl(changes)
+    accumulate(+, changes)[end]
+end
+
+function next_frequency(change_string)
+    next_frequency_impl(freq_changes_str_to_arr(change_string))
 end
 
 @testset "Day 1 Frequency Strings" begin
-    @test new_frequency("+1, -2, +3, +1") == 3
-    @test new_frequency("+1, +1, +1") == 3
-    @test new_frequency("+1, +1, -2") == 0
-    @test new_frequency("-1, -2, -3") == -6
+    @test next_frequency("+1, -2, +3, +1") == 3
+    @test next_frequency("+1, +1, +1") == 3
+    @test next_frequency("+1, +1, -2") == 0
+    @test next_frequency("-1, -2, -3") == -6
 end
 
 input = open("day1.input")
-println("Day 1 answer is ", new_frequency(read(input, String)))
+println("Day 1 answer is ", next_frequency(read(input, String)))
