4752c6adf9a930eb31e376498770b5c660d142ca
Mike Dalessio <mike.dalessio@gmail.com>
2018-12-09 16:14:02 -0500

    day 1 star 2

--- day1.jl
+++ day1.jl
@@ -1,3 +1,5 @@
+#! /usr/bin/env julia
+
 using Test
 
 function freq_changes_str_to_arr(change_string)
@@ -20,5 +22,38 @@ end
     @test next_frequency("-1, -2, -3") == -6
 end
 
-input = open("day1.input")
-println("Day 1 answer is ", next_frequency(read(input, String)))
+
+function find_repeat_frequency_impl(changes)
+    sum = 0
+    freq_set = Set(sum)
+
+    for (j, change) in enumerate(Iterators.cycle(changes))
+        sum += change
+        if in(sum, freq_set)
+            break
+        end
+        push!(freq_set, sum)
+    end
+
+    sum
+end
+
+function find_repeat_frequency(change_string)
+    find_repeat_frequency_impl(freq_changes_str_to_arr(change_string))
+end
+
+@testset "Day 1 Frequency Repeat" begin
+    @test find_repeat_frequency("+1, -2, +3, +1") == 2
+    @test find_repeat_frequency("+1, -1") == 0
+    @test find_repeat_frequency("+3, +3, +4, -2, -4") == 10
+    @test find_repeat_frequency("-6, +3, +8, +5, -6") == 5
+    @test find_repeat_frequency("+7, +7, -2, -7, -4") == 14
+end
+
+
+function input()
+    read(open("day1.input"), String)
+end
+
+println("Day 1 Star 1 answer is ", next_frequency(input()))
+println("Day 1 Star 2 answer is ", find_repeat_frequency(input()))
