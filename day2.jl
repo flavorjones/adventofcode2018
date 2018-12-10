b319b2fd172d16d76209d005814d7f69307af844
Mike Dalessio <mike.dalessio@gmail.com>
2018-12-09 21:54:39 -0500

    day 2 star 2

--- day2.jl
+++ day2.jl
@@ -36,6 +36,38 @@ function checksum(ids)
     twos * threes
 end
 
+function off_by_one_substr(id1, id2)
+    id1 = collect(id1)
+    id2 = collect(id2)
+    if (length(id1) != length(id2))
+        return nothing
+    end
+    diff = id1 - id2
+    if (length(filter(c -> c != 0, diff)) == 1)
+        rval = Char[]
+        for j = 1:length(id1)
+            if id1[j] == id2[j]
+                push!(rval, id1[j])
+            end
+        end
+        return join(rval)
+    end
+    nothing
+end
+
+function find_first_off_by_one(inputs)
+    ids = split(inputs, r"[\s]+", keepempty=false)
+    for j = 1:length(ids)
+        for k = (j+1):length(ids)
+            result = off_by_one_substr(ids[j], ids[k])
+            if result != nothing
+                return result
+            end
+        end
+    end
+    return nothing
+end
+
 @testset "Day 2" begin
     @testset "letter_count" begin
         @test letter_count("abcdef") == Dict('a' => 1, 'b' => 1, 'c' => 1, 'd' => 1, 'e' => 1, 'f' => 1)
@@ -68,8 +100,27 @@ abcdee
 ababab"
         @test checksum(input) == 12
     end
+
+    @testset "off_by_one_substr" begin
+        @test off_by_one_substr("abcde", "abcd") == nothing
+        @test off_by_one_substr("abcde", "axcye") == nothing
+        @test off_by_one_substr("fghij", "fguij") == "fgij"
+    end
+
+    @testset "find_first_off_by_one" begin
+        input = "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"
+
+        @test find_first_off_by_one(input) == "fgij"
+    end
 end
 
 input = read(open("day2.input"), String)
 
 println("Day 2 Star 1 answer is ", checksum(input))
+println("Day 2 Star 1 answer is ", find_first_off_by_one(input))
